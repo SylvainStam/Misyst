be80a4a311558dde43db4cf7154a8b837a494399
Sylvain Stam <stam.sylvain@gmail.com>
2023-06-21 23:20:43 +0200

    Add files via upload

--- js/Misyst_logo.js
+++ js/Misyst_logo.js
@@ -1,14 +1,42 @@
-const loader = new THREE.GLTFLoader();
-loader.load("./resources/models/misyst_logo/scene.gltf",
-      (gltf) => {
-        gltf.scene.scale.multiplyScalar(1 / 100); // adjust scalar factor to match your scene scale
-        gltf.scene.position.x = 20; // once rescaled, position the model where needed
-        gltf.scene.position.z = -20;
+    var camera, scene, renderer;
 
-        scene.add(gltf.scene);
-        render();
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
+        var loader;
+
+        init();
+
+        function init() {
+            renderer = new THREE.WebGLRenderer( {alpha: true, antialias: true} );
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setClearColor(0xfafdfd, 0);
+            document.body.appendChild( renderer.domElement );
+
+            var light = new THREE.PointLight( new THREE.Color( "0xffffff" ), 2, 100 );
+            light.position.set( 0,5,0 );
+            scene.add( light );
+
+            camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+            camera.position.set( 0,5,7 );
+            camera.lookAt( 0, 0, 0 );
+
+            loader = new THREE.GLTFLoader();
+            loader.load( './resources/models/misyst_logo/scene.gltf',  
+                function( gltf ) {
+                  for (var i = 0; i < 8; i++) {
+
+                    var model = gltf.scene.children[0];
+                    model.material.opacity = 0.11;
+                    model.material.transparent = true;
+              
+                    scene.add(model);
+                  }
+                }, 
+                function ( xhr ) {
+                    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+                },
+                function ( error ) {
+                    console.log( 'An Error Occurred' );
+                }
+            );
+
+            renderer.render( scene, camera );
+        }
